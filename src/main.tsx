c5345118ab67cad1cf27114c5eb3fe8f61fd4386
thedanturner <danielturner@outlook.com>
2026-07-16 11:35:33 +0100

    Added Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './styles.css';
 import './categories.css';
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+    <Analytics />
+  </StrictMode>,
+);
